ce3f657f814afe7db7bd6afde94cfd49dceac1e0
Zach Urich <re.jo@live.com>
2022-02-05 13:12:29 -0700

    hooking up prismic - wiring up to navigation

--- app/root.tsx
+++ app/root.tsx
@@ -4,12 +4,12 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
-} from "remix";
-import type { MetaFunction } from "remix";
+  ScrollRestoration,
+} from 'remix';
+import type { MetaFunction } from 'remix';
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: 'Zach Urich' };
 };
 
 export default function App() {
@@ -25,7 +25,7 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
+        {process.env.NODE_ENV === 'development' && <LiveReload />}
       </body>
     </html>
   );
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,62 @@
+import { Link, useLoaderData, useLocation } from 'remix';
+import { getNavigation, NavItem } from '../prismic';
+
+type Content = {
+  nav: NavItem[];
+};
+
+export const loader = async (): Promise<Content> => {
+  const navigation = await getNavigation();
+  return {
+    nav: [
+      {
+        id: 'home',
+        to: '/',
+        text: 'zachurich.com',
+      },
+      ...navigation,
+    ],
+  };
+};
+
 export default function Index() {
+  const cms = useLoaderData();
+  const location = useLocation();
+  console.log(location);
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <header>
+        <nav>
+          <ul>
+            {cms.nav.map((link: NavItem) => {
+              if (link.external) {
+                return (
+                  <li key={link.id}>
+                    <a href={link.to} target="_blank" rel="noreferrer">
+                      {link.text}
+                    </a>
+                  </li>
+                );
+              }
+              return (
+                <li key={link.id}>
+                  <Link
+                    to={link.to}
+                    aria-current={
+                      location.pathname === link.to ? 'page' : undefined
+                    }
+                  >
+                    {link.text}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      <main id="main-content">
+        <h1>Welcome to Remix</h1>
+      </main>
     </div>
   );
 }
